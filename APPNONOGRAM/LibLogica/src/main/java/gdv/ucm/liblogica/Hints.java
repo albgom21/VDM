348134bf7b4501aea83200759324d8f703f235b9
Alejao03 <alejao03@ucm.es>
2022-09-27 11:54:53 +0200

    Avanzada Logica
    Co-Authored-By: albgom21 <62644841+albgom21@users.noreply.github.com>
    Co-Authored-By: davidrodriguezz <62748674+davidrodriguezz@users.noreply.github.com>

--- APPNONOGRAM/LibLogica/src/main/java/gdv/ucm/liblogica/Hints.java
+++ APPNONOGRAM/LibLogica/src/main/java/gdv/ucm/liblogica/Hints.java
@@ -1,28 +1,72 @@
 package gdv.ucm.liblogica;
 
+import javafx.util.Pair;
+
 public class Hints {
     // Attributes
     private int horizontalHints[][];
     private int verticalHints[][];
+    private int depthCounter [];
+    private boolean ant[];
 
     public Hints(Board b){
+        //Vectors
         horizontalHints = new int[b.getWidth()][(b.getHeight()+1)/2];
         verticalHints = new int[b.getHeight()][(b.getWidth()+1)/2];
-        boolean a = true;
-        int n = 0;
-        int depth = 0;
-        for (int i = 0; i < b.getWidth(); ++i) {
-            for (int j = 0; j < b.getHeight(); ++j) {
+        ant = new boolean[b.getWidth()];
+        depthCounter = new int[b.getWidth()];
+
+        //Comprobar si se inicializa depth a 0 y ant a false (Preguntar)
+
+        //Counter
+        int depthx = 0;
+        for (int i = 0; i < b.getHeight(); ++i) {
+            depthx = 0;
+            for (int j = 0; j < b.getWidth(); ++j) {
                 boolean sol = b.getCell(i,j).getisSol();
                 if(sol){
-                    n++;
+                    verticalHints[i][depthx]++;
+                    ant[j] = true;
+                    horizontalHints[depthCounter[j]][j]++;
                 }
-                else if (!sol && n!=0){
-                    verticalHints[i][depth] = n;
-                    depth++;
-                    n=0;
+                else // !Sol
+                {
+                    if(verticalHints[i][depthx] >= 1)
+                        depthx++;
+                    if (ant[j])
+                        depthCounter[j]++;
+
+                    ant[j] = false;
                 }
             }
         }
+    }
+
+    public Pair<Integer,Integer> Check(Board b)
+    {
+        int counterBlue = 0;
+        int counterRed = 0;
+
+        for (int i = 0; i < b.getHeight(); ++i) {
+            for (int j = 0; j < b.getWidth(); ++j) {
+                boolean sol = b.getCell(i,j).getisSol();
+                Cell cell = b.getCell(i,j);
+                if(sol)
+                {
+                    if (cell.getState() == CellState.GREY || cell.getState() == CellState.WHITE)
+                        counterBlue++;
+                }
+                else // !Sol
+                {
+                    if(cell.getState() == CellState.BLUE) {
+                        cell.setState(CellState.RED); //No estamos seguros (Preguntar)
+                        counterRed++;
+                    }
+                }
+            }
+        }
+
+        Pair<Integer,Integer> a = new Pair<>(counterBlue,counterRed);
+        return a;
     }
 }
